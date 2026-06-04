79bb7cc409e43653ad48105e21c1a84d6efc1c14
jcarbor2911 <jcarbor2911@gmail.com>
2026-06-04 11:14:31 +0200

    Jueves 04/06

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from '../components/footer/footer';
+import { Header } from '../components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
